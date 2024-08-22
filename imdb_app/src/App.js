9fcc1563bc48c582f1938694f9e82d28f0e6cde7
Raj-Prasanth02 <rajolimani99@gmail.com>
2024-08-22 22:10:09 +0530

    save changes

--- imdb_app/src/App.js
+++ imdb_app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("pushing the code to check the repository");
+  console.log("pushing the code to check the repo's");
   return (
     <div className="App">
       <header className="App-header">
