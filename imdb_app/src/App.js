346a5dad59bf43ec46c4b18d2350c8e445c952fa
Raj Olimani <rajolimani@Rajs-MacBook-Air.local>
2024-08-22 22:03:18 +0530

    modified

--- imdb_app/src/App.js
+++ imdb_app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("pushing the code to check the repo");
   return (
     <div className="App">
       <header className="App-header">
